279e67a71ec0d6e5d92ffac1e5a34ad102f1f76f
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2025-10-03 13:07:05 +0530

    I want to change this

--- Pattern/Upper_right_triangle.js
+++ Pattern/Upper_right_triangle.js
@@ -1,21 +1,4 @@
 
-
-// let input = require('readline-sync');
-// let N = input.question("Enter your number: ");
-
-// for (let i = 1; i <= N; i++) {
-//     let st = "";
-//     for (let j = 0; j < i; j++) {
-//         st += "*"
-//     }
-//     console.log(st);
-// }
-
-
-
-
-
-
 let input = require('readline-sync');
 let N = input.question("Enter your number: ");
 
