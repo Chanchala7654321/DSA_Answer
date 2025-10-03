1372148c607e33ad407f81faa3e32c317d3cf424
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2025-10-03 12:32:58 +0530

    Add Upper Left and Upper Right Triangle Pattern Programs

---
+++ Pattern/Upper_left_triangle.js
@@ -0,0 +1,13 @@
+let input = require('readline-sync');
+let N = input.question("Enter your number: ");
+
+for (let i = 1; i <= N; i++) {
+    let st = " ";
+    for (let j = 0; j < N - i; j++) {
+        st += " "
+    }
+    for (let k = 0; k < i; k++) {
+        st += "*"
+    }
+    console.log(st);
+}
---
+++ Pattern/Upper_right_triangle.js
@@ -0,0 +1,28 @@
+
+
+// let input = require('readline-sync');
+// let N = input.question("Enter your number: ");
+
+// for (let i = 1; i <= N; i++) {
+//     let st = "";
+//     for (let j = 0; j < i; j++) {
+//         st += "*"
+//     }
+//     console.log(st);
+// }
+
+
+
+
+
+
+let input = require('readline-sync');
+let N = input.question("Enter your number: ");
+
+for (let i = 1; i <= N; i++) {
+    let st = " ";
+    for (let j = 0; j < i; j++) {
+        st += "*"
+    }
+    console.log(st);
+}
