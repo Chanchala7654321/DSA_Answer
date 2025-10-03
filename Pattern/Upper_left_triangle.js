f8c697a77857b82f98d2f64e233e7de26b743afe
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2025-10-03 12:39:41 +0530

    I want to update Upper file

--- Pattern/Upper_left_triangle.js
+++ Pattern/Upper_left_triangle.js
@@ -1,6 +1,6 @@
 let input = require('readline-sync');
 let N = input.question("Enter your number: ");
-
+// Chanchala 
 for (let i = 1; i <= N; i++) {
     let st = " ";
     for (let j = 0; j < N - i; j++) {
